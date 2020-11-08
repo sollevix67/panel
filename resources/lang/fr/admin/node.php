27f8225bc341730d21f08681e15fc72ea994ff28
sollevix67 <7199110+sollevix67@users.noreply.github.com>
2020-11-08 12:03:40 +0100

    translating panel to French

--- resources/lang/fr/admin/node.php
+++ resources/lang/fr/admin/node.php
@@ -14,8 +14,8 @@ return [
     ],
     'notices' => [
         'allocations_added' => 'Les allocations ont bien été ajoutées à ce nœud.',
-        'node_deleted' => 'Le nœud a été supprimé du panneau avec succès.',
-        'location_required' => 'Vous devez avoir au moins un emplacement configuré avant de pouvoir ajouter un nœud à ce panneau.',
+        'node_deleted' => 'Le nœud a été supprimé du Panel avec succès.',
+        'location_required' => 'Vous devez avoir au moins un emplacement configuré avant de pouvoir ajouter un nœud à ce Panel.',
         'node_created' => 'Nouveau nœud créé avec succès. Vous pouvez configurer automatiquement le démon sur cette machine en visitant l\'onglet \'Configuration \'. <strong> Avant de pouvoir ajouter des serveurs, vous devez d\'abord attribuer au moins une adresse IP et un port.</strong>',
         'node_updated' => 'Les informations sur le nœud ont été mises à jour. Si des paramètres du démon ont été modifiés, vous devrez le redémarrer pour que ces modifications prennent effet.',
         'unallocated_deleted' => 'Suppression de tous les ports non alloués pour <code>:ip</code>.',
